f3007fe506ce0939d03eaf6ac32cf32d0421af9a
Daniel Ayzenshteyn <ayzendan@post.bgu.ac.il>
2023-05-01 23:10:03 +0300

    Success! Q3b && Q4

--- src/q3.ts
+++ src/q3.ts
@@ -1,5 +1,19 @@
-import {  Exp, Program } from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
+import {
+    CExp,
+    CondClause,
+    CondExp,
+    Exp,
+    IfExp, isAppExp, isAtomicExp, isCExp, isCondExp, isDefineExp, isExp, isIfExp, isLetExp, isLitExp, isProcExp,
+    isProgram, makeAppExp,
+    makeBoolExp,
+    makeCondExp, makeDefineExp,
+    makeIfExp, makeProcExp,
+    makeProgram,
+    Program
+} from "./L31-ast";
+import {Result, makeFailure, makeOk, bind} from "../shared/result";
+import {isEmpty, map} from "ramda";
+import {first, rest} from "../shared/list";
 
 
 /*
@@ -8,4 +22,47 @@ Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+    makeOk(rewriteAllCond(exp))
+
+export const rewriteCondToIf = (condExp: CondExp): IfExp => {return rewriteCondClause(condExp.condclauses)}
+
+export const rewriteCondClause = (condClauses: CondClause[]): IfExp => {
+    if(!isEmpty(condClauses)){
+        if (condClauses.length === 2){
+            return makeIfExp(condClauses[0].test, condClauses[0].then[0], condClauses[1].then[0])
+        }
+        else{
+            return makeIfExp(first(condClauses).test,
+                (first(condClauses)).then[0],
+                rewriteCondClause(rest(condClauses)))
+        }
+    }
+    else{
+        return makeIfExp(makeBoolExp(true),makeBoolExp(true),makeBoolExp(true))
+    }
+}
+
+const rewriteAllCond = (exp: Program | Exp): Program | Exp =>
+    isExp(exp) ? rewriteAllCondExp(exp) :
+        isProgram(exp) ? makeProgram(map(rewriteAllCondExp, exp.exps)) :
+            exp;
+
+const rewriteAllCondExp = (exp: Exp): Exp =>
+    isCExp(exp) ? rewriteAllCondExpCExp(exp) :
+        isDefineExp(exp) ? makeDefineExp(exp.var, rewriteAllCondExpCExp(exp.val)) :
+            exp;
+
+
+
+const rewriteAllCondExpCExp = (exp: CExp): CExp =>
+    isAtomicExp(exp) ? exp :
+        isLitExp(exp) ? exp :
+            isIfExp(exp) ? makeIfExp(rewriteAllCondExpCExp(exp.test),
+                    rewriteAllCondExpCExp(exp.then),
+                    rewriteAllCondExpCExp(exp.alt)) :
+                isAppExp(exp) ? makeAppExp(rewriteAllCondExpCExp(exp.rator),
+                        map(rewriteAllCondExpCExp, exp.rands)) :
+                    isProcExp(exp) ? makeProcExp(exp.args, map(rewriteAllCondExpCExp, exp.body)) :
+                        isLetExp(exp) ? exp:
+                            isCondExp(exp) ? rewriteAllCondExpCExp(rewriteCondToIf(exp)) :
+                                exp;
--- src/q4.ts
+++ src/q4.ts
@@ -1,10 +1,63 @@
-import { Exp, Program } from '../imp/L3-ast';
-import { Result, makeFailure } from '../shared/result';
+import { map } from "ramda";
+import { Exp, Program, isProgram, isBoolExp, isNumExp, isVarRef, isPrimOp, isProcExp, isIfExp, isAppExp, isDefineExp, PrimOp, CExp } from '../imp/L3-ast';
+import { Result, makeFailure, makeOk, bind, mapResult, safe2 } from '../shared/result';
 
 /*
 Purpose: Transform L2 AST to Python program string
 Signature: l2ToPython(l2AST)
 Type: [Parsed | Error] => Result<string>
 */
-export const l2ToPython = (exp: Exp | Program): Result<string>  => 
-    makeFailure("TODO");
+export const l2ToPython = (exp: Exp | Program): Result<string> => {
+    if (isProgram(exp)) {
+        return bind(mapResult(l2ToPython, exp.exps), exps => makeOk(exps.join("\n")));
+    }
+    if (isBoolExp(exp)) {
+        return makeOk(exp.val ? 'True' : 'False');
+    }
+    if (isNumExp(exp)) {
+        return makeOk(exp.val.toString());
+    }
+    if (isVarRef(exp)) {
+        return makeOk(exp.var);
+    }
+    if (isDefineExp(exp)) {
+        return bind(l2ToPython(exp.val), val => makeOk(`${exp.var.var} = ${val}`));
+    }
+    if (isIfExp(exp)) {
+        return safe3((test: string, then: string, alt: string) => makeOk(`(${then} if ${test} else ${alt})`))
+        (l2ToPython(exp.test), l2ToPython(exp.then), l2ToPython(exp.alt));
+    }
+    if (isPrimOp(exp)) {
+        return makeOk(convertPrimOp(exp.op));
+    }
+    if (isProcExp(exp)) {
+        return bind(l2ToPython(exp.body[exp.body.length - 1]), body => makeOk("(" + "lambda " +
+            map((p) => p.var, exp.args).join(",") + " : " + body + ")"));
+    }
+    if (isAppExp(exp)) {
+        if (isPrimOp(exp.rator)) {
+            return primOpApp2Python(exp.rator, exp.rands);
+        }
+        return safe2((rator: string, rands: string[]) => makeOk(`${rator}(${rands.join(",")})`))
+        (l2ToPython(exp.rator), mapResult(l2ToPython, exp.rands));
+    }
+    return makeFailure("Never");
+}
+
+
+const convertPrimOp = (op: string): string =>
+    op === "=" || op === "eq?" ? "==" : op === "number?" ? "(lambda x : type(x) == int or type (x) == float)" : op === "boolean?" ? "(lambda x : type(x) == bool)" : op;
+
+const primOpApp2Python = (rator: PrimOp, rands: CExp[]): Result<string> =>
+    rator.op === "not"
+        ? bind(l2ToPython(rands[0]), (rand: string) => makeOk("(not " + rand + ")"))
+        : rator.op === "number?" || rator.op === "boolean?"
+            ? bind(l2ToPython(rands[0]), (rand: string) => makeOk(`${convertPrimOp(rator.op)}(${rands[0]})`))
+            : bind(mapResult(l2ToPython, rands), (rands) =>
+                makeOk("(" + rands.join(" " + convertPrimOp(rator.op) + " ") + ")")
+            );
+
+const safe3 =
+    <T1, T2, T3, T4>(f: (x: T1, y: T2, z: T3) => Result<T4>) =>
+        (xr: Result<T1>, yr: Result<T2>, zr: Result<T3>): Result<T4> =>
+            bind(xr, (x: T1) => bind(yr, (y: T2) => bind(zr, (z: T3) => f(x, y, z))));
